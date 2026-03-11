1910141b98a8b1ae38795c932374340857566eb6
FWLSH <indservertripura@gmail.com>
2026-03-11 22:47:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -453,6 +453,6 @@ def direct_uid(uid):
     return jsonify(player_data)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5001)
 
 #made by juli_dvrma
